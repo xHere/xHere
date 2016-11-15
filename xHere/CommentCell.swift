ad3c6f76d84fd19b1da4097ed8a189fad54b038e
dharabavishi <dharabavishi@Ruchits-MacBook-Pro.local>
2016-11-14 19:44:40 -0800

    Add Detail and Discovery View controllers

---
+++ xHere/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  xHere
+//
+//  Created by Ruchit Mehta on 11/14/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ xHere/XHEREDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  XHEREDetailViewController.swift
+//  xHere
+//
+//  Created by Ruchit Mehta on 11/14/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+
+class XHEREDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
