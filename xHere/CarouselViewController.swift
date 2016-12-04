e33dc3125e1e977f2d529678fe83f2a0fd4f2193
jsingh <waheguru~8>
2016-12-04 14:04:18 -0800

    Changes in carousel to make it look better

--- xHere/CarouselViewController.swift
+++ xHere/CarouselViewController.swift
@@ -11,6 +11,8 @@ import iCarousel
 
 class CarouselViewController: UIViewController,iCarouselDataSource, iCarouselDelegate {
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var carousel: iCarousel!
     var bounties : [XHERBounty]?
@@ -18,11 +20,24 @@ class CarouselViewController: UIViewController,iCarouselDataSource, iCarouselDel
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.layoutIfNeeded()
         carousel.type = .coverFlow2
         carousel.delegate = self
         carousel.dataSource = self
         carousel.currentItemIndex  = currentIndex
+        carousel.isPagingEnabled = true
+        //        setupBackground(currentIndex: currentIndex)
+        if(currentIndex == 0){
+            intialSetup()
+        }
+        profileImageView.clipsToBounds = true
+//        profileImageView.layer.borderWidth = 1
+        profileImageView.layer.masksToBounds = false
+//        profileImageView.layer.borderColor = UIColor.black.cgColor
+        profileImageView.layer.cornerRadius = profileImageView.frame.height/2
+        profileImageView.clipsToBounds = true
     }
+    
     
     public func numberOfItems(in carousel: iCarousel) -> Int {
         if let bounties =  bounties {
@@ -36,14 +51,11 @@ class CarouselViewController: UIViewController,iCarouselDataSource, iCarouselDel
             return 0
         }
     }
-
+    
     func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
-        var label: UILabel
         var itemView: UIImageView
-        
         if let view = view as? UIImageView {
             itemView = view
-            label = itemView.viewWithTag(1) as! UILabel
         } else {
             itemView = UIImageView(frame: CGRect(x: 0, y: 0, width:  400, height: 400))
             if let bounty = bounties?[index] {
@@ -51,30 +63,51 @@ class CarouselViewController: UIViewController,iCarouselDataSource, iCarouselDel
                     let url = URL(string: (media.mediaData?.url)!)
                     itemView.setImageWith(url!)
                     itemView.clipsToBounds = true
+                    itemView.backgroundColor = UIColor.clear
                 }
-                itemView.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-                
-                
             }
-            
         }
-
-//        label.text = "@\()"
-        print(bounties?[index])
-        if let claimedUserEmail = bounties?[index].claimedByUser?.username {
-            print(claimedUserEmail)
-        }
-    
         print(bounties?[index].claimedByUser!.email)
-        self.userNameLabel.text = bounties?[index].claimedByUser?.username!
+//        self.userNameLabel.text = bounties?[index].claimedByUser?.username!
         return itemView
+    }
+    
+    func intialSetup(){
+        userNameLabel.text = bounties?[0].claimedByUser?.firstName
+        profileImageView.setImageWith((bounties?[0].claimedByUser?.profileImageUrl)!)
+        if let bounty = bounties?[0] {
+            if let media = bounty.mediaArray?[0]{
+                let url = URL(string: (media.mediaData?.url)!)
+                backgroundImageView.setImageWith(url!)
+                let blurEffect = UIBlurEffect(style: .light)
+                let blurview = UIVisualEffectView(effect: blurEffect)
+                blurview.frame = backgroundImageView.bounds
+                //        blurview.backgroundColor = UIColor.red
+                backgroundImageView.addSubview(blurview)
+            }
+        }
+    }
+    
+    func setupBackground(currentIndex : Int ){
+        let currentView = carousel.currentItemView as! UIImageView
+        backgroundImageView.image = currentView.image
+        backgroundImageView.subviews.forEach({ $0.removeFromSuperview() })
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurview = UIVisualEffectView(effect: blurEffect)
+        blurview.frame = backgroundImageView.bounds
+        backgroundImageView.addSubview(blurview)
+        
     }
     
     func carouselCurrentItemIndexDidChange(_ carousel: iCarousel) {
         print(carousel.currentItemIndex)
         var currentIndex = carousel.currentItemIndex
         userNameLabel.text = bounties?[currentIndex].claimedByUser?.firstName
+        print(bounties?[0].claimedByUser?.profileImageUrl)
+        profileImageView.setImageWith((bounties?[currentIndex].claimedByUser?.profileImageUrl)!)
+        setupBackground(currentIndex: currentIndex)
     }
+    
     
     func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
         if (option == .spacing) {
@@ -82,5 +115,5 @@ class CarouselViewController: UIViewController,iCarouselDataSource, iCarouselDel
         }
         return value
     }
-
+    
 }
