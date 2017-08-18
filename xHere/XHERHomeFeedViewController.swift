290a96b895a1d6bad8ee88914f240ac766ceec55
Michael <realq86@gmail.com>
2017-08-17 21:32:54 -0700

    Re importing SVProgressHUD

--- xHere/XHERHomeFeedViewController.swift
+++ xHere/XHERHomeFeedViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class XHERHomeFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, XHERNearByClaimedViewCellDelegate, XHERHomeFeedVCModelDelegate  {
     
