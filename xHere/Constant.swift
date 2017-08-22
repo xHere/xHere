f8a60a0f6174b55ee10b22556c24a9bbf035bf34
Michael <realq86@gmail.com>
2017-08-21 18:55:11 -0700

    Start using constant file for global constant and keys

---
+++ xHere/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  xHere
+//
+//  Created by Chi Hwa Michael Ting on 8/21/17.
+//  Copyright © 2017 Developer. All rights reserved.
+//
+
+import Foundation
+
+
+let xHereDateFormatter = DateFormatter()
